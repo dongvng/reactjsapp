094081c2758784f729248a0b2a8337c7244b4462
dongNguyen <dongvn666@gmail.com>
2019-03-14 13:57:57 +0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import Todo from './components/main/todo/todo'
+import Header from './components/header/header'
+import Aside from './components/aside/aside'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Todo />
+        <Aside />
       </div>
     );
   }
