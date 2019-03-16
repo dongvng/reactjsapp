7adb125b5f6221a3332f8f25f9108eb88e215224
dongNguyen <dongvn666@gmail.com>
2019-03-16 23:30:12 +0700

    Updates

---
+++ precache-manifest.d28a875b11048e7d8fc4e7d4ab4f1cea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b010332697be0858d823dc6e6a924db",
+    "url": "/dongNguyenViet/reactjsapp/static/media/user-regular.1b010332.svg"
+  },
+  {
+    "revision": "1e8a2cf9173583f9ee505b6dd76b450f",
+    "url": "/dongNguyenViet/reactjsapp/static/media/plus-solid.1e8a2cf9.svg"
+  },
+  {
+    "revision": "35184bace5be0e3dc27f97a4a1bded57",
+    "url": "/dongNguyenViet/reactjsapp/static/media/done-checkmark.35184bac.svg"
+  },
+  {
+    "revision": "769e19238be77712bb37fb1ede696db8",
+    "url": "/dongNguyenViet/reactjsapp/static/media/archive-solid.769e1923.svg"
+  },
+  {
+    "revision": "c9967bc54e0697657fa4",
+    "url": "/dongNguyenViet/reactjsapp/static/js/runtime~main.e7ac69b9.js"
+  },
+  {
+    "revision": "52fea76df1e42213c050",
+    "url": "/dongNguyenViet/reactjsapp/static/js/main.5eda7ffa.chunk.js"
+  },
+  {
+    "revision": "ba61119a383f37b83e4b",
+    "url": "/dongNguyenViet/reactjsapp/static/js/2.897c2415.chunk.js"
+  },
+  {
+    "revision": "52fea76df1e42213c050",
+    "url": "/dongNguyenViet/reactjsapp/static/css/main.d52271bc.chunk.css"
+  },
+  {
+    "revision": "ba61119a383f37b83e4b",
+    "url": "/dongNguyenViet/reactjsapp/static/css/2.876b64ff.chunk.css"
+  },
+  {
+    "revision": "9c3888c0cc3124f516c93c9dc4c19aa3",
+    "url": "/dongNguyenViet/reactjsapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dongNguyenViet/reactjsapp/precache-manifest.d28a875b11048e7d8fc4e7d4ab4f1cea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dongNguyenViet/reactjsapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
